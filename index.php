55e28d5014ee6bed116a029c327eda14a4a1b0d1
Necromide <wilhelm.goerlitz95@gmail.com>
2023-09-15 13:51:44 +0200

    Own File for css

--- index.php
+++ index.php
@@ -5,49 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Codebasis Webshop</title>
-    <style>
-        /* Allgemeine Stile */
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-            color: #333;
-        }
+    <link rel="stylesheet" type="text/css" href="style.css">
 
-        h1, p {
-            text-align: center;
-        }
-
-        /* Zentrierter Container für den Inhalt */
-        .container {
-            max-width: 800px;
-            margin: 50px auto;
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
-        }
-
-        /* Stil für den Button */
-        .btn {
-            display: block;
-            width: 200px;
-            height: 40px;
-            margin: 20px auto;
-            background-color: #007BFF;
-            text-align: center;
-            border-radius: 4px;
-            color: white;
-            font-weight: bold;
-            line-height: 40px;
-            text-decoration: none;
-        }
-
-        .btn:hover {
-            background-color: #0056b3;
-        }
-    </style>
 </head>
 
 <body>
